ee3d8087dbb0e8f6623705553a7e904a7691c6de
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-07-24 14:44:07 -0500

    Abstracted portfolio header

---
+++ src/components/PortfolioHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PortfolioHeader() {
+
+    return (
+        <h1 style={{
+                    marginTop: '10px',
+                    marginBottom: '40px'
+            }}>
+                Projects 
+        </h1>
+    )
+}
+
+export default PortfolioHeader;
