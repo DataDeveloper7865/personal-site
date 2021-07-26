272aea1debdadcbe05f7ae9bb865c6e007d6759d
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-07-26 16:30:00 -0500

    updated about

---
+++ src/components/About/AboutHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col } from 'antd';
+
+function AboutHeader() {
+
+    return (
+        <Col>
+            <h1> Software Developer</h1>
+
+            <p> I am a former biochemist turned software developer.</p>
+            <p> I write code in Javascript and Python.</p>
+            <p> I use the following technologies.</p>
+        </Col>
+    )
+}
+
+export default AboutHeader;
