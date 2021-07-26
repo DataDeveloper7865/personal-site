2573a4a949629588916e12616a4fa5ebd85c2e34
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-07-26 16:27:14 -0500

    modified image params

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,37 @@
-import React from 'react';
-import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
-import profilePic from '../static/profile-pic.JPG'
+import React from "react";
+import { GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
+import profilePic from "../static/profile-pic.JPG";
 
 function Home() {
+  return (
+    <div class="body-content">
+      <div>
+        <img
+          class="prof-pic"
+          width={208}
+          height={208}
+          src={profilePic}
+          alt="profile pic"
+        />
+      </div>
 
+      <p style={{ marginTop: "25px" }}>Hi, I'm Steve. I love writing code.</p>
+      <p style={{ marginTop: "25px" }}>
+        Check out my Github and Linkedin below.
+      </p>
 
-    return (
-        <div class="body-content">
-           <div>
-                <img
-                    width={208}
-                    height={200}
-                    src={profilePic}
-                    alt="profile pic" 
-                />
-            </div>
-            
-
-            <p style={{marginTop: '25px'}}>
-                Hi, I'm Steve. I love writing code.
-            </p>
-            <p style={{marginTop: '25px'}}>
-                Check out my Github and Linkedin below.
-            </p>
-
-            <div>
-                <a href="https://github.com/DataDeveloper7865" >
-                    <GithubOutlined style={{marginRight: '50px', fontSize: '200%', color: 'black'}}/>
-                </a>
-                <a href="https://www.linkedin.com/in/stephen-whelan-gne/">
-                    <LinkedinOutlined style={{ fontSize: '200%', color: 'black'}}/>
-                </a>
-
-            </div>
-        </div>
-    ) 
+      <div>
+        <a href="https://github.com/DataDeveloper7865">
+          <GithubOutlined
+            style={{ marginRight: "50px", fontSize: "200%", color: "black" }}
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/stephen-whelan-gne/">
+          <LinkedinOutlined style={{ fontSize: "200%", color: "black" }} />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
